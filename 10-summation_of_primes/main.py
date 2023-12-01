41dd33681989d97dd9b15b5ef4d72fef973998eb
santiago lopez de haro <santiagolhgpro@gmail.com>
2023-11-30 23:20:26 -0300

    Agregue algunos comentarios

--- 10-summation_of_primes/main.py
+++ 10-summation_of_primes/main.py
@@ -4,7 +4,7 @@ not_visited = [True] * max
 for i in range(2, max, 2):
     not_visited[i] = False
 
-# We loop through each value, and everytime we encounter a prime we mark all it´s multiplications as not prime, so we don´t have to take it into account in the sum
+# We loop through each value, and everytime we encounter a prime we mark all it´s multiplications as not prime, so we don´t have to take it into account in the sum ( We contruct the loop in such a way you will always encounter a prime in the if statement)
 
 for i in range(3, max, 2):
     if( not_visited[i] ):
