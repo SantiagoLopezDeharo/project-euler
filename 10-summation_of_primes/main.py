a7c14d79b0401bf0e2e5be9697971e0fa11dc1ac
santiago lopez de haro <santiagolhgpro@gmail.com>
2023-11-30 23:19:20 -0300

    Termine el ejercicio 10

---
+++ 10-summation_of_primes/main.py
@@ -0,0 +1,14 @@
+max = 2000000
+suma = 2
+not_visited = [True] * max
+for i in range(2, max, 2):
+    not_visited[i] = False
+
+# We loop through each value, and everytime we encounter a prime we mark all it´s multiplications as not prime, so we don´t have to take it into account in the sum
+
+for i in range(3, max, 2):
+    if( not_visited[i] ):
+        suma = suma + i
+        for j in range(i, max, i ):
+            not_visited[j] = False
+print(suma)
