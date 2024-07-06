ef3b2ae04e84d41edaec5948db99befd7740a9a0
Santiago Lopez de haro <santiagolhgpro@gmail.com>
2024-07-06 20:59:09 -0300

    Termine el desafio 33, hice unas cosas medio extravagantes

--- 33-digit_cancelling_fractions/main.py
+++ 33-digit_cancelling_fractions/main.py
@@ -1,12 +1,66 @@
-from numpy import np
+import numpy as np
 
-for i in range(11, 100):
-    for j in range(10, i):
-        igual = -1
-        for ii in str(i):
-            for jj in str(j):
-                if ii == jj:
-                    igual = i
-        if igual != -1:
-                  
-        np.isclose(a, b, rtol=1e-05, atol=1e-08, equal_nan=False)
+numbers =    [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
+numbersStr = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"]
+
+results = []
+
+for i in range(len(numbers)):
+    for j in range(i):
+        if (numbersStr[i][0] == numbersStr[j][1] and numbersStr[i][1] != "0"):
+            if (int(numbersStr[j][0])/int(numbersStr[i][1]) == numbers[j] / numbers[i] ):
+                results.append([numbers[j], numbers[i]])
+
+print(results)
+res = [1, 1]
+
+for i in results:
+    res[0] *= i[0]
+    res[1] *= i[1]
+
+print(res)
+
+max = int( np.trunc(np.sqrt(res[1])))
+suma = 2
+not_visited = []
+
+for i in range(max+1):
+    not_visited.append(True)
+    
+for i in range(2, max, 2):
+    not_visited[i] = False
+
+# We loop through each value, and everytime we encounter a prime we mark all it´s multiplications as not prime, so we don´t have to take it into account in the sum ( We contruct the loop in such a way you will always encounter a prime in the if statement)
+primos = [2]
+for i in range(3, max, 2):
+    if( not_visited[i] ):
+        primos.append(i)
+        for j in range(i, max, i ):
+            not_visited[j] = False
+
+
+#   I create a procedure to get the prime descomposition of a given number with a given array of prime numbers
+
+def factorization(primos, n):
+    potencias = []
+    for i in range(len(primos)):
+        potencias.append(0)
+    
+    for i in range(len(primos)):
+        while(n % primos[i] == 0):
+            potencias[i] = potencias[i] + 1
+            n = n/primos[i]
+    return potencias
+
+potenciasNumerator   = factorization(primos, res[0])
+potenciasDenominator = factorization(primos, res[1])
+
+answer = res[1]
+
+for i in range(len(primos)):
+    if (potenciasNumerator[i] < potenciasDenominator[i]):
+        answer /= primos[i] ** potenciasNumerator[i]
+    else:
+        answer /= primos[i] ** potenciasDenominator[i]
+
+print(f" The answer to the challenge is : {answer}")
