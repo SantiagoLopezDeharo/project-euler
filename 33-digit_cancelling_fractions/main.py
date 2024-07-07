82953310a7e7b8a7e54e01d572008460f38e3300
Santiago Lopez de haro <santiagolhgpro@gmail.com>
2024-07-06 23:44:26 -0300

    Agregue unos comentarios para que sea mas legible

--- 33-digit_cancelling_fractions/main.py
+++ 33-digit_cancelling_fractions/main.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# we load in memory the numbers we are gonna use, since they are a small amount we can do it
+
 numbers =    [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
 numbersStr = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99"]
 
@@ -7,14 +9,14 @@ results = []
 
 for i in range(len(numbers)):
     for j in range(i):
-        if (numbersStr[i][0] == numbersStr[j][1] and numbersStr[i][1] != "0"):
-            if (int(numbersStr[j][0])/int(numbersStr[i][1]) == numbers[j] / numbers[i] ):
+        if (numbersStr[i][0] == numbersStr[j][1] and numbersStr[i][1] != "0"): #If they share a digit
+            if (int(numbersStr[j][0])/int(numbersStr[i][1]) == numbers[j] / numbers[i] ): # If we remove the digit that is the same, does it have the same value ?
                 results.append([numbers[j], numbers[i]])
 
 print(results)
 res = [1, 1]
 
-for i in results:
+for i in results: # We multiply all the found fractions and we represent it as a vector
     res[0] *= i[0]
     res[1] *= i[1]
 
@@ -56,6 +58,8 @@ potenciasNumerator   = factorization(primos, res[0])
 potenciasDenominator = factorization(primos, res[1])
 
 answer = res[1]
+
+# Using the prime descomposition we now can find the smalles expresion of the fraction by dividing them into the prime numbers that they share
 
 for i in range(len(primos)):
     if (potenciasNumerator[i] < potenciasDenominator[i]):
