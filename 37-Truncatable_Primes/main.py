a260bed2f9556f28b0d92b7a14a291036bf1ee5e
santiago lopez de haro <santiagolhgpro@gmail.com>
2024-07-07 22:24:29 -0300

    Agregue comentarios para clarificar la logica

--- 37-Truncatable_Primes/main.py
+++ 37-Truncatable_Primes/main.py
@@ -22,36 +22,30 @@ truncable_primes = 0
 
 ans = 0
 
-not_included = {1, 2, 3, 5, 7}
-
 for i in primes:    # O( n * log(n) ) being n the amount of prime numbers under 1000000
     if not_visited[i] and len(str(i)) > 1:
-        flag = True
+        flag = True # We keep a flag for knowing if the prime in cuestion have the desire properties
         p = str(i)
         not_visited[i] = False
-        temp = [i]
+        
         for j in range(len(p)-1):
-            aux = p[1:]
+            aux = p[1:]  # We remove the first digit
             not_visited[int(aux)] = False
             flag = flag and (int(aux) in primes) # O(log(n))
             p = aux
-            if not int(p) in temp and not int(p) in not_included:
-                temp.append(int(p))
             if not flag:
                 break
         p = str(i)
+
         for j in range(len(p)-1):
-            aux = p[:len(p)-1]
+            aux = p[:len(p)-1]  # We remove the last digit
             not_visited[int(aux)] = False
             flag = flag and (int(aux) in primes) # O(log(n))
             p = aux
-            if not int(p) in temp and not int(p) in not_included:
-                temp.append(int(p))
             if not flag:
                 break
         if flag:
-            print(temp)
-            truncable_primes += 1
+            truncable_primes += 1 # We keep count of the truncable primes we find
             ans += i
 
 print(f" The amount of truncable primes under {max} is: {truncable_primes} and their sum is: {ans}")
