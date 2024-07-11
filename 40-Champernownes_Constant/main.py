a1903b4e9d17d20f6ad9b48f6603ddcfd4dce037
Santiago Lopez de haro <santiagolhgpro@gmail.com>
2024-07-11 19:58:06 -0300

    Cambie el desafio 40 para que sea mas memory friendly

--- 40-Champernownes_Constant/main.py
+++ 40-Champernownes_Constant/main.py
@@ -1,16 +1,21 @@
-number = ""
 
 maxDigit = 1000000
 
-# Since to compute until the one million diigit we dont need much iterations we will iterate until we get there (Notice it will be a lot less than one million iterations, since we weill be adding multiple digits in each iteration) 
-i = 0
-while len(number) <= maxDigit:
-    number += str(i) # We create the irrational number until the maximum digit we are going to consult
-    i += 1
+wantedDigits = [1, 10, 100, 1000, 10000, 100000, 1000000]
 
 ans = 1
 
-for i in range(len(str(maxDigit))):
-    ans *= int(number[10**i])   # We compute the answer for the challenge now that we have all the data needed
+# Since to compute until the one million diigit we dont need much iterations we will iterate until we get there (Notice it will be a lot less than one million iterations, since we weill be adding multiple digits in each iteration) 
+n = 1
+indx = 1
+while indx <= maxDigit:
+    for i in str(n):
+        if indx in wantedDigits: # if we arrived to a desired digit we multiply by it
+            ans *= int(i)
+        indx += 1
+    n += 1
 
 print(ans)
+
+# Time complexity: O(n) with n begin the maximum digit to calculate to
+# Memory complexity: O(1)
