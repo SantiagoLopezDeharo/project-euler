0c4991accbc9ccdcb20bba9e6b6c58d3b7b5a727
Santiago Lopez de haro <santiagolhgpro@gmail.com>
2024-07-25 11:58:00 -0300

    Termine el desafio 44, no llegue a ninguna optimizacion matematica, entonces simplemente hice un brute force controlado y logre que en python termine en menos de 1 minuto

---
+++ 44-Pentagon_Numbers/main.py
@@ -0,0 +1,25 @@
+def is_pentagonal(x):
+    """Check if a number x is a pentagonal number"""
+    n = (1 + (1 + 24 * x) ** 0.5) / 6
+    return n == int(n)
+
+def pentagonal(n):
+    """Generate the n-th pentagonal number"""
+    return n * (3 * n - 1) // 2
+
+def find_min_pentagonal_pair():
+    pentagonals = []
+    n = 1
+    # We brute force the answer
+    while True:
+        P_n = pentagonal(n)
+        pentagonals.append(P_n)
+        for P_j in pentagonals[:-1]:
+            P_k = P_n
+            if is_pentagonal(P_k - P_j) and is_pentagonal(P_k + P_j):
+                return P_k - P_j
+        n += 1
+
+D = find_min_pentagonal_pair()
+
+print("The answer is: " + str(D))
